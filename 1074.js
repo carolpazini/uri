469c5bb82b8aca973f9ec6b79f3ffa5995db88d5
Caroline Pazini Cavalcante - CCV <caroline.cavalcante@iteris.local>
2019-04-12 15:11:10 -0300

    desafios

--- 1074.js
+++ 1074.js
@@ -1,21 +1,26 @@
 var input = require('fs').readFileSync('1074.txt', 'utf8');
 var lines = input.split('\r\n');
 
-var N = parseInt( lines[0]);
-var X = parseInt( lines[1]);
 
-while (N>10000);
-if (X%2 == 0 && X>0){
-    console.log ("EVEN POSITIVE");
+var N = parseInt( lines[0]);
+var i = parseInt( lines[1]);
+var X = i++;
+
+for (i > 1; i++;){ 
+
+
+        if (X%2 == 0 && X>0){
+            console.log ("EVEN POSITIVE");
+        }
+        else if(X%2 == 0 && X<0){
+            console.log ("EVEN NEGATIVE");
+        }
+        else if(X%2 != 0 && X>0){
+            console.log ("ODD POSITIVE");
+        }
+        else if(X%2 != 0 && X<0){
+            console.log ("ODD NEGATIVE");
+        }
+        else if(X == 0)
+            console.log ("NULL");
 }
-else if(X%2 == 0 && X<0){
-    console.log ("EVEN NEGATIVE");
-}
-else if(X%2 != 0 && X>0){
-    console.log ("ODD POSITIVE");
-}
-else if(X%2 != 0 && X<0){
-    console.log ("ODD NEGATIVE");
-}
-else if(X == 0)
-    console.log ("NULL");
