3704701605dd397a95866717cbe08b83f45495db
Caroline Pazini Cavalcante - CCV <caroline.cavalcante@iteris.local>
2019-04-15 10:50:45 -0300

    1272

--- 1272.js
+++ 1272.js
@@ -1,2 +1,19 @@
 var input = require('fs').readFileSync('1272.txt', 'utf8');
 var lines = input.split('\r\n');
+
+var testes = parseInt(lines[0]);
+var frase = new Array(); //todas as frases
+
+	for (var i = 0; i < testes; i++){
+	
+        var frase = lines[i+1].toString().split(" "); 
+        var palavra;
+        var msg = ""; 
+
+	for (var j = 0; j < frase.length; j++){
+        palavra = frase[j];
+        msg = msg + palavra.substring(0,1);
+
+    }
+        console.log(msg);
+    }
